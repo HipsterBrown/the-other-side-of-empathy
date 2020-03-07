4403f60f2b0113a206ad13636b6eaecc872108f8
HipsterBrown <headhipster@hipsterbrown.com>
2020-03-06 22:03:56 -0500

    use purgecss to reduce production css

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,22 @@
 const postcssPresetEnv = require("postcss-preset-env");
 const postcssImport = require("postcss-import");
 const cssnano = require("cssnano");
-const postcssMedia = require("postcss-custom-media");
 const tailwind = require("tailwindcss");
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./_includes/**/*.hbs"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
 
 module.exports = {
   plugins: [
     tailwind,
     postcssImport(),
-    postcssMedia(),
     postcssPresetEnv(),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
     cssnano()
   ]
 };
