48942375c203c5a2023a3fc2bfd260df71722fae
HipsterBrown <headhipster@hipsterbrown.com>
2020-03-06 21:50:19 -0500

    replace basscss with tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,14 @@ const postcssPresetEnv = require("postcss-preset-env");
 const postcssImport = require("postcss-import");
 const cssnano = require("cssnano");
 const postcssMedia = require("postcss-custom-media");
+const tailwind = require("tailwindcss");
 
 module.exports = {
-  plugins: [postcssImport(), postcssMedia(), postcssPresetEnv(), cssnano()]
+  plugins: [
+    tailwind,
+    postcssImport(),
+    postcssMedia(),
+    postcssPresetEnv(),
+    cssnano()
+  ]
 };
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: [
+        "Avenir Next",
+        "Avenir",
+        "Helvetica Neue",
+        "Helvetica",
+        "sans-serif"
+      ]
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
